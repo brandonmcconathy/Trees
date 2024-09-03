941b4f3f6c304086fa46e4f5a6b77b6c66882629
Brandon McConathy <brandonmcconathy4321@gmail.com>
2024-09-02 21:32:31 -0700

    Set up binary tree class

--- binaryTree.cpp
+++ binaryTree.cpp
@@ -1,5 +1,17 @@
 #include "binaryTree.h"
 
-binaryTree::binaryTree() {
+binaryTree::binaryTree(int val) {
+
+}
+
+binaryTree::binaryTree(int val, binaryTree* left) {
+
+}
+
+binaryTree::binaryTree(int val, binaryTree* right) {
+
+}
+
+binaryTree::binaryTree(int val, binaryTree* left, binaryTree* right) {
 
 }
--- binaryTree.h
+++ binaryTree.h
@@ -4,5 +4,12 @@ class binaryTree {
 private:
 
 public:
-	binaryTree ();
+	int val;
+	binaryTree* left;
+	binaryTree* right;
+
+	binaryTree(int val);
+	binaryTree(int val, binaryTree* left);
+	binaryTree(int val, binaryTree* right);
+	binaryTree(int val, binaryTree* left, binaryTree* right);
 };
