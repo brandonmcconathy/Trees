38b0db7ef157325119cff5fcf3e06f582cca41af
Brandon McConathy <brandonmcconathy4321@gmail.com>
2024-09-04 10:48:55 -0700

    Added preorder traversal

--- main.cpp
+++ main.cpp
@@ -3,21 +3,14 @@
 #include "binaryTree.h"
 #include "printList.h"
 
-static void traverse(std::vector<int>& list, binaryTree* node) {
-    list.push_back(node->val);
-    if (node->left) {
-        traverse(list, node->left);
-    }
-    if (node->right) {
-        traverse(list, node->right);
-    }
-}
+// Traversals
+#include "preTraversal.h"
 
 int main()
 {
     binaryTree* tree = new binaryTree(4, new binaryTree(7, new binaryTree(9), new binaryTree(1)), new binaryTree(22));
     
     std::vector<int> list;
-    traverse(list, tree);
+    preTraverse(list, tree);
     printList(list);
 }
---
+++ preTraversal.h
@@ -0,0 +1,7 @@
+#pragma once
+#include "binaryTree.h"
+#include <vector>
+
+static void traverse(std::vector<int>& list, binaryTree* node);
+
+void preTraverse(std::vector<int>& list, binaryTree* root);
