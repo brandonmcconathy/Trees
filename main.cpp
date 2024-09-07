8654e21db2c7a3a4cc8cda2158c18f255ef8b4a6
Brandon McConathy <brandonmcconathy4321@gmail.com>
2024-09-06 17:52:33 -0700

    Cleaned up main function

--- main.cpp
+++ main.cpp
@@ -12,15 +12,29 @@ int main()
 {
     binaryTree* tree = new binaryTree(4, new binaryTree(7, new binaryTree(9), new binaryTree(1)), new binaryTree(22));
     
+    // Traversals
+    std::cout << "Traversals" << std::endl;
+    std::cout << std::endl;
+
+    // Preorder
+    std::cout << "Preorder: " << std::endl;
     std::vector<int> list;
     preTraverse(list, tree);
     printList(list);
+    std::cout << std::endl;
 
+    // Inorder
+    std::cout << "Inorder:" << std::endl;
     list = {};
     inTraverse(list, tree);
     printList(list);
+    std::cout << std::endl;
 
+    // Postorder
+    std::cout << "Postorder:" << std::endl;
     list = {};
     postTraverse(list, tree);
     printList(list);
+    std::cout << std::endl;
+    std::cout << std::endl;
 }
