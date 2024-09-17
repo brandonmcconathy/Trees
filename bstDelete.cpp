980ceab26d114686b9a9399d073c2ab77b6a7910
Brandon McConathy <brandonmcconathy4321@gmail.com>
2024-09-16 20:08:08 -0700

    Added first case of BST delete

--- bstDelete.cpp
+++ bstDelete.cpp
@@ -2,5 +2,41 @@
 #include "binaryTree.h"
 
 void bstDelete(binaryTree* tree, int val) {
+	binaryTree* currNode = tree;
+	binaryTree* prevNode;
+
+	// Puts currNode on node we need to delete and prevNode on node before
+	while (currNode->val != val) {
+		if (val <= currNode->val) {
+			if (currNode->left) {
+				prevNode = currNode;
+				currNode = currNode->left;
+			}
+			else {
+				return;
+			}
+		}
+		else {
+			if (currNode->right) {
+				prevNode = currNode;
+				currNode = currNode->right;
+			}
+			else {
+				return;
+			}
+		}
+	}
+
+	// Case where currNode has no children
+	if (currNode->left == nullptr && currNode->right == nullptr) {
+		if (prevNode->left->val == val) {
+			prevNode->left = nullptr;
+		}
+		else {
+			prevNode->right = nullptr;
+		}
+	}
+
+
 	return;
 }
