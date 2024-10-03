95e8c742358157fa2773c7374c540c6f3b9d55cb
Brandon McConathy <brandonmcconathy4321@gmail.com>
2024-10-03 16:37:28 -0700

    Started add method

--- trie.cpp
+++ trie.cpp
@@ -1,6 +1,12 @@
 #include "trie.h"
 #include "trieNode.h"
+#include <string>
 
 Trie::Trie() {
+	chars.assign(26, 0);
+	return;
+}
+
+void Trie::add(std::string word) {
 	return;
 }
--- trie.h
+++ trie.h
@@ -1,12 +1,16 @@
 #pragma once
 #include "trieNode.h"
 #include <vector>
+#include <string>
 
 class Trie
 {
 private:
+	std::vector<bool> chars;
 
 public:
 	std::vector<trieNode*> children;
+
 	Trie();
+	void add(std::string word);
 };
